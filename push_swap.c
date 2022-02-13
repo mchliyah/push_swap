ed615bb805df686e259c6c811bb3f04c0af4a3e1
Mohammed Chliyah <mchliyah@e2r2p8.1337.ma>
2022-02-13 23:02:25 +0100

    v2-2

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mchliyah <mchliyah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 15:21:21 by mchliyah          #+#    #+#             */
-/*   Updated: 2022/02/13 22:22:25 by mchliyah         ###   ########.fr       */
+/*   Updated: 2022/02/13 22:52:39 by mchliyah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ int	main(int ac, char **av)
 {
 	t_stack	a;
 	t_stack	b;
-	int		*arr;
+	int	*arr;
 
 	a = initial_stack(a, ac, av);
 	b = initial_stack(b, ac, NULL);
-	arr = malloc(sizeof(int *) * a.l);
 	if (a.l <= 3)
 		handle_3(&a, &b);
 	else if (a.l > 3 && a.l <= 5)
 		handle_5(&a, &b);
 	if (a.l > 5)
 	{
+		arr = malloc(sizeof(int *) * a.l);
 		arr = ft_memcpy(arr, a.dt, a.l * sizeof(int));
 		int	i;
 		int	j;
@@ -51,10 +51,25 @@ int	main(int ac, char **av)
 			i++;
 		}
 		med = arr[a.l / 2];
-		
+		while (a.l > 1)
+		{
+			while (b.l <= a.l / 2)
+			{
+				if (a.dt[a.l - 1] < med)
+					ft_push(&a, &b, 'b');
+				else
+				{
+					if (b.l > 1 && b.dt[b.l - 1] < b.dt[0])
+						ft_rotat(&a, &b, 1);
+					else
+						ft_rotat(&a, NULL, 1);
+				}
+			}
+			med = arr[a.l / 2];
+		}
 	}
 	printf("\033[0;34mafter sort\n");
-	for (int n = a.top; n >= 0 ; n--)
+	for (int n = a.l - 1; n >= 0 ; n--)
 		printf("%d\n", a.dt[n]);
 	free (a.dt);
 	free (b.dt);
