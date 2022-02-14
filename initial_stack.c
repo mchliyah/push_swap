e0c7f8d3736689dc98e0c6ddbc989cb13a6289fb
Mohammed Chliyah <mchliyah@e1r2p11.1337.ma>
2022-02-14 19:20:40 +0100

    reset

--- initial_stack.c
+++ initial_stack.c
@@ -6,7 +6,7 @@
 /*   By: mchliyah <mchliyah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 17:20:12 by mchliyah          #+#    #+#             */
-/*   Updated: 2022/02/14 18:08:25 by mchliyah         ###   ########.fr       */
+/*   Updated: 2022/02/11 19:33:23 by mchliyah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,21 @@ t_stack	initial_stack(t_stack stack, int ac, char **av)
 {
 	int		i;
 
-	stack.sortd = 0;
 	if (av)
 	{
+		stack.top = ac - 2;
 		stack.l = ac - 1;
 		stack.dt = malloc(sizeof(int *) * stack.l);
 		ft_check_av(ac, av);
 		i = -1;
 		while (++i < stack.l)
 			stack.dt[i] = ft_atoi(av[ac - i - 1]);
-		for (int n = stack.l - 1; n >= 0 ; n--)
+		for (int n = stack.top; n >= 0 ; n--)
 			printf("%d\n", stack.dt[n]);
 	}
 	else
 	{
+		stack.top = ac - 2;
 		stack.l = 0;
 		stack.dt = malloc(sizeof(int *) * stack.l);
 	}
